68ab1d241348527e80122324792ed95013455a6a
farnatasha <2020898386@student.uitm.edu.my>
2022-04-24 21:20:26 +0800

    3rd commit

--- handle_form.php
+++ handle_form.php
@@ -10,7 +10,8 @@
     <?php #Script handle form
     echo "<p> Thank you, <b>".$_POST["name"].
     "</b> for the following comments: <br/>".
-    $_POST["comments"]."<p>We will replt to you at <i>".
+    $_POST["comments"]."<p>We will reply to you at <i>".
     $_POST["email"]." </i></p>";
+    ?>
 </body>
 </html>
